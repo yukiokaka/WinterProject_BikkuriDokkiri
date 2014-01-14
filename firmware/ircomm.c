df9185af44e17aae0d44de44231924b0aff85042
Y.Okamoto <yukiokaka@gmail.com>
2014-01-14 11:09:03 +0900

    refactoring ircomm.c

--- firmware/ircomm.c
+++ firmware/ircomm.c
@@ -12,36 +12,37 @@ volatile int reader_count = 0;
 volatile char reader_count_start_state = 0;
 volatile unsigned long second_counter = 0;
 
-static unsigned long micros(void) 
+static inline unsigned long micros(void) 
 {
     return second_counter;
 }
     
 
-
 void ircomm_init(void)
 {
 	LPC_SYSCON->SYSAHBCLKCTRL |=  (1 << 9);
-    LPC_TMR32B0 -> PR = (3*2)-1;			
-    LPC_TMR32B0 -> MR3 = 26-1;			
+    LPC_TMR32B0 -> PR = (3 * 2) - 1;			
+    LPC_TMR32B0 -> MR3 = 26 - 1;			
     LPC_TMR32B0 -> MCR &= ~(7 << 9);	
     LPC_TMR32B0 -> MCR |=  (3 << 9);	
-    NVIC -> ISER[0] |= (1<<18);
+    NVIC -> ISER[0] |= (1 << 18);
     LPC_TMR32B0->TCR = 1;
 }
 
-void ircomm_send(char *buf)
+int ircomm_send(char *buf)
 {
     unsigned char send_data_ir_bar;
-    send_data_ir++;
-    send_data_ir_bar = ~send_data_ir;
-    send_data = (send_data_ir_bar << 8 | send_data_ir);
-    xprintf("%d:%d:%d\n", send_data_ir, send_data_ir_bar, send_data);
-    while(reader_count_start_state);
-    LPC_GPIO1 -> DATA &= ~_BV(3);
-    reader_count = 0;
-    reader_count_start_state = 1;
-    
+    if(reader_count_start_state)
+        return -1;
+    else {
+        send_data_ir++;
+        send_data_ir_bar = ~send_data_ir;
+        send_data = (send_data_ir_bar << 8 | send_data_ir);        
+        LPC_GPIO1 -> DATA &= ~_BV(3);
+        reader_count = 0;
+        reader_count_start_state = 1;
+        return 0;
+    }
 }
 
 int ircomm_recv(char *buf)
@@ -55,18 +56,18 @@ int ircomm_recv(char *buf)
      ans = 0 ;
      t   = 0 ;
           // リーダ部のチェックを行う
-     if(LPC_GPIO1->MASKED_ACCESS[1 << 0] == 0) {
+     if(LPC_GPIO1 -> MASKED_ACCESS[1 << 0] == 0) {
           t = micros() ;                        
           // 現在の時刻(us)を得る
-          while (LPC_GPIO1->MASKED_ACCESS[1 << 0]== 0) ;	
+          while (LPC_GPIO1 -> MASKED_ACCESS[1 << 0] == 0);	
           // 1(ON)になるまで待つ
-          t = micros() - t ;	
+          t = micros() - t;	
           // 0(OFF)の部分をはかる
      }
      
      // リーダ部有りなら処理する(4.5ms以上の0にて判断する)
      if (t >= 4500) {
-         i = 0 ;
+         i = 0;
          while(LPC_GPIO1->MASKED_ACCESS[1 << 0] == 1) {
              timeout++;
              if(timeout == 10000)
@@ -76,37 +77,39 @@ int ircomm_recv(char *buf)
          // データ部の読み込み
          while (1) {
              timeout = 0;
-             while(LPC_GPIO1->MASKED_ACCESS[1 << 0] == 0){
+             while(LPC_GPIO1->MASKED_ACCESS[1 << 0] == 0) {
               timeout++;
               if(timeout == 10000)
                   break;              
              }	          
              // OFF部分は読み飛ばす
-             t = micros() ;
+             t = micros();
              timeout = 0;
-             while(LPC_GPIO1->MASKED_ACCESS[1 << 0] == 1)  {
+             while(LPC_GPIO1->MASKED_ACCESS[1 << 0] == 1) {
                  timeout++;
                  if(timeout == 10000)
                      break;              
              }             
              // 0(FF)になるまで待つ
-             t = micros() - t ;					
+             t = micros() - t;					
              // 1(ON)部分の長さをはかる
-             if (t >= 1000)  IRbit[i] = (char)0x31 ;	// ON部分が長い
-             else            IRbit[i] = (char)0x30 ;	// ON部分が短い
+             if (t >= 1000)  IRbit[i] = (char)0x31;	// ON部分が長い
+             else            IRbit[i] = (char)0x30;	// ON部分が短い
              i++ ;
-             if (i == 16) break ;				// 1byte読込んだら終了
+             if (i == 16) break;				// 1byte読込んだら終了
           }
 
          data = ((IRbit[7] == 0x31) << 7) | ((IRbit[6]  == 0x31) << 6) | ((IRbit[5] == 0x31) << 5) | ((IRbit[4]  == 0x31) << 4) | ((IRbit[3] == 0x31) << 3) | ((IRbit[2] == 0x31) << 2) | ((IRbit[1]  == 0x31)<< 1)| ((IRbit[0] == 0x31) << 0);
          data_bar = ((IRbit[15] == 0x31) << 7) | ((IRbit[14]  == 0x31) << 6) | ((IRbit[13] == 0x31) << 5) | ((IRbit[12]  == 0x31) << 4) | ((IRbit[11] == 0x31) << 3) | ((IRbit[10] == 0x31) << 2) | ((IRbit[9]  == 0x31)<< 1)| ((IRbit[8] == 0x31) << 0);
 
-         if(data == 255-data_bar)
-             return data;
-         else
+         if(data == 255-data_bar) {
+             *buf = data;
              return 0;
+         }
+         else
+             return -1;
      }
-     return 0;
+     return -2;
 }
 
 
