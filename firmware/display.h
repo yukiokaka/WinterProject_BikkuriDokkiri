597b170811208ad97bd3fb676664b959c8d0bf1a
ueda12 <d.ueda.1117@gmail.com>
2014-01-15 19:29:18 +0900

    added a dot picture of '0'

--- firmware/display.h
+++ firmware/display.h
@@ -2,9 +2,6 @@
 #define DISPLAY_H_
 
 void matrix_init();
-extern short DotPicture_1[];
-extern short DotPicture_2[];
-extern short DotPicture_3[];
-extern short DotPicture_4[];
+extern short DotPicture[][];
 void display(short array[]);
 #endif
